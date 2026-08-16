522d3afaf8f8b91b4944825f515e124a9f01a813
david@david-m5 <3200210+davidjenni@users.noreply.github.com>
2026-08-15 23:07:32 -0700

    use non-prod .env for CI

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+env_file="${1:-lan.prod.env}"
+echo ".. Deploying using env file: $env_file"
+
 echo ">>-- Current running containers:" && \
 docker container ls && \
 echo ">>-- Building and pulling latest images..." && \
-docker compose --env-file lan.prod.env pull && \
-docker compose --env-file lan.prod.env build && \
+docker compose --env-file "$env_file" pull && \
+docker compose --env-file "$env_file" build && \
 echo ">>-- Deploying new versions..." && \
-docker compose --env-file lan.prod.env stop && \
-docker compose --env-file lan.prod.env up -d --wait && \
+docker compose --env-file "$env_file" stop && \
+docker compose --env-file "$env_file" up -d --wait && \
 echo ">>-- Deployment complete. Current containers:" && \
 docker container ls -a && \
 echo ">>-- Current container stats:" && \
-docker compose --env-file lan.prod.env stats --no-stream
+docker compose --env-file "$env_file" stats --no-stream
 
 pihole_container="${PIHOLE_CONTAINER:-ns-pihole-1}"
 
