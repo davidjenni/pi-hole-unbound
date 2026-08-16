334b4f419958fea5780c77a1696237db90851f3d
David JENNI <3200210+davidjenni@users.noreply.github.com>
2026-08-15 23:27:09 -0700

    feat: deploy script prompts and sets pihole webUI password (#12)
    * feat: deploy script prompts and sets pihole webUI password
    * allow securely setting admin web gui password via env var or interactively
    * validate with deploy script in CI
    * use non-prod .env for CI
    * correctly chain setting pw

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+env_file="${1:-lan.prod.env}"
+echo ">>.. Deploying using env file: $env_file"
+pihole_container="${PIHOLE_CONTAINER:-ns-pihole-1}"
+
 echo ">>-- Current running containers:" && \
 docker container ls && \
 echo ">>-- Building and pulling latest images..." && \
-docker compose --env-file lan.prod.env pull && \
-docker compose --env-file lan.prod.env build && \
+docker compose --env-file "$env_file" pull && \
+docker compose --env-file "$env_file" build && \
 echo ">>-- Deploying new versions..." && \
-docker compose --env-file lan.prod.env stop && \
-docker compose --env-file lan.prod.env up -d --wait && \
+docker compose --env-file "$env_file" stop && \
+docker compose --env-file "$env_file" up -d --wait && \
 echo ">>-- Deployment complete. Current containers:" && \
 docker container ls -a && \
 echo ">>-- Current container stats:" && \
-docker compose --env-file lan.prod.env stats --no-stream
+docker compose --env-file "$env_file" stats --no-stream && \
+echo ">>-- Setting Pi-hole web UI password..." && {
+  if [[ -n "${PIHOLE_WEBUI_PASSWORD:-}" ]]; then
+    docker exec -i "$pihole_container" sh -c 'read -r pw; pihole setpassword "$pw"' <<<"$PIHOLE_WEBUI_PASSWORD"
+  elif [[ -t 0 ]]; then
+    docker exec -it "$pihole_container" pihole setpassword
+  else
+    echo "PIHOLE_WEBUI_PASSWORD must be set when running non-interactively." >&2
+    exit 1
+  fi
+}
