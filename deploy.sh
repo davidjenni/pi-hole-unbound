f09715fde6ff35a5e4eb9b3cf79bde2077d8fee7
david@david-m5 <3200210+davidjenni@users.noreply.github.com>
2026-08-15 22:53:43 -0700

    validate deploy script in CI

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,15 @@ docker compose --env-file lan.prod.env up -d --wait && \
 echo ">>-- Deployment complete. Current containers:" && \
 docker container ls -a && \
 echo ">>-- Current container stats:" && \
-docker compose --env-file lan.prod.env stats --no-stream && {
-  pihole_container="${PIHOLE_CONTAINER:-ns-pihole-1}"
+docker compose --env-file lan.prod.env stats --no-stream
 
-  if [[ -n "${PIHOLE_WEBUI_PASSWORD:-}" ]]; then
-    docker exec -i "$pihole_container" sh -c 'read -r pw; pihole setpassword "$pw"' <<<"$PIHOLE_WEBUI_PASSWORD"
-  elif [[ -t 0 ]]; then
-    docker exec -it "$pihole_container" pihole -a -p
-  else
-    echo "PIHOLE_WEBUI_PASSWORD must be set when running non-interactively." >&2
-    exit 1
-  fi
-}
+pihole_container="${PIHOLE_CONTAINER:-ns-pihole-1}"
+
+if [[ -n "${PIHOLE_WEBUI_PASSWORD:-}" ]]; then
+  docker exec -i "$pihole_container" sh -c 'read -r pw; pihole setpassword "$pw"' <<<"$PIHOLE_WEBUI_PASSWORD"
+elif [[ -t 0 ]]; then
+  docker exec -it "$pihole_container" pihole setpassword
+else
+  echo "PIHOLE_WEBUI_PASSWORD must be set when running non-interactively." >&2
+  exit 1
+fi
